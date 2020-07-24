31c0f13b2a175dac2a3b8f20fa747b9cb2faaa80
YunJ1e <yunjiewang8214314@gmail.com>
2020-07-24 17:21:47 -0400

    Queue and Stack

---
+++ DataStructure/QueueAndStack.py
@@ -0,0 +1,38 @@
+"""
+Updated: 2020/07/24
+Author: Yunjie Wang
+"""
+
+"""
+Queue
+There are two ways implementing this data structure and one built-in feature in the Python(deque)
+"""
+# This first one is using list
+class Queue(object):
+	def __init__(self):
+		pass
+	def __len__(self):
+		pass
+	def enqueue(self):
+		pass
+	def dequeue(self):
+		pass
+	def is_empty(self):
+		pass
+	def front(self):
+		pass
+
+# This second one is using linked-list
+# class Queue(object):
+# 	def __init__(self):
+# 		pass
+# 	def __len__(self):
+# 		pass
+# 	def enqueue(self):
+# 		pass
+# 	def dequeue(self):
+# 		pass
+# 	def is_empty(self):
+# 		pass
+# 	def front(self):
+# 		pass
