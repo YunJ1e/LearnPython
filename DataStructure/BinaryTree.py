35c35e81226a37268d7dd6d9f75f3e6d1720e18b
YunJ1e <yunjiewang8214314@gmail.com>
2020-08-02 17:18:00 -0400

    Traverse by level

--- DataStructure/BinaryTree.py
+++ DataStructure/BinaryTree.py
@@ -12,6 +12,15 @@ class Binary_Tree(object):
 	def __init__(self):
 		self.root = None
 
+	def random_tree(self):
+		self.root = _TreeNode(10)
+		self.root.left = _TreeNode(5)
+		self.root.right = _TreeNode(15)
+		self.root.left.left = _TreeNode(2)
+		self.root.left.right = _TreeNode(7)
+		self.root.right.left = _TreeNode(12)
+		self.root.right.right = _TreeNode(20)
+
 	def pre_order_traverse(self):
 		res = []
 		self.pre_order_traverse_helper(self.root, res)
@@ -48,14 +57,39 @@ class Binary_Tree(object):
 		self.post_order_traverse_helper(cur.right, res)
 		res.append(cur.value)
 
+	def get_tree_height(self, curr):
+		if not curr:
+			return 0
+		return 1 + max(self.get_tree_height(curr.left), self.get_tree_height(curr.right))
+
+	def traverse_by_level(self):
+		curr = [self.root]
+		next = []
+		line = []
+		while curr:
+			curr_node = curr.pop(0)
+			if curr_node.left:
+				next.append(curr_node.left)
+			if curr_node.right:
+				next.append(curr_node.right)
+			line.append(curr_node.value)
+			if not curr:
+				print(line)
+				line = []
+				if next:
+					curr = next
+					next = []
+
+
+
+
+
+
+
 a = Binary_Tree()
-a.root = _TreeNode(10)
-a.root.left = _TreeNode(5)
-a.root.right = _TreeNode(15)
-a.root.left.left = _TreeNode(2)
-a.root.left.right = _TreeNode(7)
-a.root.right.left = _TreeNode(12)
-a.root.right.right = _TreeNode(20)
+a.random_tree()
 a.pre_order_traverse()
 a.in_order_traverse()
 a.post_order_traverse()
+print(a.get_tree_height(a.root))
+a.traverse_by_level()
