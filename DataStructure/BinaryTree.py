4a044c8e009778a5d1f913bed16a182b1b9260da
YunJ1e <yunjiewang8214314@gmail.com>
2020-08-06 23:56:57 -0400

    Code Reconstruct

--- DataStructure/BinaryTree.py
+++ DataStructure/BinaryTree.py
@@ -18,8 +18,8 @@ class Binary_Tree(object):
 		self.root.right = _TreeNode(15)
 		self.root.left.left = _TreeNode(2)
 		self.root.left.right = _TreeNode(7)
-		self.root.right.left = _TreeNode(12)
-		self.root.right.right = _TreeNode(20)
+		# self.root.right.left = _TreeNode(12)
+		# self.root.right.right = _TreeNode(20)
 
 	def pre_order_traverse(self):
 		res = []
@@ -62,37 +62,55 @@ class Binary_Tree(object):
 			return 0
 		return 1 + max(self.get_tree_height(curr.left), self.get_tree_height(curr.right))
 
-	def traverse_by_level(self):
-		curr = [self.root]
-		next = []
-		line = []
-		while curr:
-			curr_node = curr.pop(0)
-			if curr_node.left:
-				next.append(curr_node.left)
-			if curr_node.right:
-				next.append(curr_node.right)
-			line.append(curr_node.value)
-			if not curr:
-				print(line)
-				line = []
-				if next:
-					curr = next
-					next = []
+def traverse_by_level(root):
+	curr = [root]
+	next = []
+	line = []
+	while curr:
+		curr_node = curr.pop(0)
+		if curr_node.left:
+			next.append(curr_node.left)
+		if curr_node.right:
+			next.append(curr_node.right)
+		line.append(curr_node.value)
+		if not curr:
+			print(line)
+			line = []
+			if next:
+				curr = next
+				next = []
 
-	def upside_down(self):
-		# Given a binary tree where all the right nodes are either leaf nodes with a sibling or empty
-		# flip upside down and the original right node is left node right now
-		# 	     1                         4
-		# 	   / \             	         / \
-		#     2  3                      5  2
-		# 	/ \                           / \
-		#  4  5                          3  1
-		pass
+def upside_down(root):
+	# Given a binary tree where all the right nodes are either leaf nodes with a sibling or empty
+	# flip upside down and the original right node is left node right now
+	# 	     1                         4
+	# 	   / \             	         / \
+	#     2  3                      5  2
+	# 	/ \                           / \
+	#  4  5                          3  1
+	if not root:
+		return root
+	if not root.left and not root.right:
+		return root
+	new_root = upside_down(root.left)
+	root.left.left = root.right
+	root.left.right = root
+	root.left = None
+	root.right = None
+	return new_root
+
+
+
+
+
+
+
 a = Binary_Tree()
 a.random_tree()
 a.pre_order_traverse()
 a.in_order_traverse()
 a.post_order_traverse()
 print(a.get_tree_height(a.root))
-a.traverse_by_level()
+traverse_by_level(a.root)
+b = upside_down(a.root)
+traverse_by_level(b)
