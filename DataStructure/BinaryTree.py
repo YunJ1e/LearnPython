fa1c2f8ae0d8e7f56df4b9287c1b40f7fffb5a95
YunJ1e <yunjiewang8214314@gmail.com>
2020-08-08 15:41:48 -0400

    Several Recursion

--- DataStructure/BinaryTree.py
+++ DataStructure/BinaryTree.py
@@ -1,5 +1,5 @@
 """
-Updated: 2020/07/30
+Updated: 2020/08/07
 Author: Yunjie Wang
 """
 class _TreeNode(object):
@@ -7,60 +7,121 @@ class _TreeNode(object):
 		self.value = x
 		self.left = None
 		self.right = None
+		self.left_total = 0
+		self.right_total = 0
 
-class Binary_Tree(object):
-	def __init__(self):
-		self.root = None
+def random_tree():
+	root = _TreeNode(1)
+	root.left = _TreeNode(3)
+	root.right = _TreeNode(4)
+	root.left.left = _TreeNode(5)
+	root.left.right = _TreeNode(6)
+	# root.left.right.left = _TreeNode(999)
+	root.right.left = _TreeNode(5)
+	root.right.right = _TreeNode(8)
+	return root
 
-	def random_tree(self):
-		self.root = _TreeNode(10)
-		self.root.left = _TreeNode(5)
-		self.root.right = _TreeNode(15)
-		self.root.left.left = _TreeNode(2)
-		self.root.left.right = _TreeNode(7)
-		# self.root.right.left = _TreeNode(12)
-		# self.root.right.right = _TreeNode(20)
+def pre_order_iter(root):
+	aStack = [(root, 1)]
+	aList = []
+	while aStack:
+		node, count = aStack.pop()
+		if count == 1:
+			aList.append(node.value)
+			aStack.append((node, count + 1))
+			if node.left:
+				aStack.append((node.left, 1))
+		if count == 2:
+			aStack.append((node, count + 1))
+			if node.right:
+				aStack.append((node.right, 1))
+		if count == 3:
+			continue
+	print(aList)
 
-	def pre_order_traverse(self):
-		res = []
-		self.pre_order_traverse_helper(self.root, res)
-		print(res)
+def pre_order_traverse(root):
+	res = []
+	pre_order_traverse_helper(root, res)
+	print(res)
 
-	def pre_order_traverse_helper(self, cur, res):
-		if not cur:
-			return
-		res.append(cur.value)
-		self.pre_order_traverse_helper(cur.left, res)
-		self.pre_order_traverse_helper(cur.right, res)
+def pre_order_traverse_helper(cur, res):
+	if not cur:
+		return
+	res.append((cur.value, cur.left_total, cur.right_total))
+	pre_order_traverse_helper(cur.left, res)
+	pre_order_traverse_helper(cur.right, res)
 
-	def in_order_traverse(self):
-		res = []
-		self.in_order_traverse_helper(self.root, res)
-		print(res)
+def in_order_iter(root):
+	aStack = [(root, 1)]
+	aList = []
+	while aStack:
+		node, count = aStack.pop()
+		if count == 1:
+			aStack.append((node, count + 1))
+			if node.left:
+				aStack.append((node.left, 1))
+		if count == 2:
+			aList.append(node.value)
+			aStack.append((node, count + 1))
+			if node.right:
+				aStack.append((node.right, 1))
+		if count == 3:
+			continue
+	print(aList)
 
-	def in_order_traverse_helper(self, cur, res):
-		if not cur:
-			return
-		self.in_order_traverse_helper(cur.left, res)
-		res.append(cur.value)
-		self.in_order_traverse_helper(cur.right, res)
+def in_order_traverse(root):
+	res = []
+	in_order_traverse_helper(root, res)
+	print(res)
 
-	def post_order_traverse(self):
-		res = []
-		self.post_order_traverse_helper(self.root, res)
-		print(res)
+def in_order_traverse_helper(cur, res):
+	if not cur:
+		return
+	in_order_traverse_helper(cur.left, res)
+	res.append(cur.value)
+	in_order_traverse_helper(cur.right, res)
 
-	def post_order_traverse_helper(self, cur, res):
-		if not cur:
-			return
-		self.post_order_traverse_helper(cur.left, res)
-		self.post_order_traverse_helper(cur.right, res)
-		res.append(cur.value)
+def post_order_iter(root):
+	aStack = [(root, 1)]
+	aList = []
+	while aStack:
+		node, count = aStack.pop()
+		if count == 1:
+			aStack.append((node, count + 1))
+			if node.left:
+				aStack.append((node.left, 1))
+		if count == 2:
+			aStack.append((node, count + 1))
+			if node.right:
+				aStack.append((node.right, 1))
+		if count == 3:
+			aList.append(node.value)
+	print(aList)
 
-	def get_tree_height(self, curr):
-		if not curr:
-			return 0
-		return 1 + max(self.get_tree_height(curr.left), self.get_tree_height(curr.right))
+def post_order_traverse(root):
+	res = []
+	post_order_traverse_helper(root, res)
+	print(res)
+
+def post_order_traverse_helper(cur, res):
+	if not cur:
+		return
+	post_order_traverse_helper(cur.left, res)
+	post_order_traverse_helper(cur.right, res)
+	res.append(cur.value)
+
+def get_tree_height(curr):
+	if not curr:
+		return 0
+	return 1 + max(get_tree_height(curr.left), get_tree_height(curr.right))
+
+def get_tree_size(curr):
+	if not curr:
+		return 0
+	left = get_tree_size(curr.left)
+	right = get_tree_size(curr.right)
+
+	return 1 + left + right
 
 def traverse_by_level(root):
 	curr = [root]
@@ -99,18 +160,45 @@ def upside_down(root):
 	root.right = None
 	return new_root
 
+def longest_consec(root, par, longest_length_at_par):
+	if not root:
+		return longest_length_at_par
+	size = 1
+	if par and root.value == par.value + 1:
+		size = longest_length_at_par + 1
+	return max(longest_length_at_par, longest_consec(root.left, root, size), longest_consec(root.right, root, size))
+
+def num_of_left_node(curr_node):
+	if not curr_node:
+		return 0
+	left = num_of_left_node(curr_node.left)
+	right = num_of_left_node(curr_node.right)
+	curr_node.left_total = left
+	return 1 + left + right
+
+def num_of_right_node(curr_node):
+	if not curr_node:
+		return 0
+	left = num_of_right_node(curr_node.left)
+	right = num_of_right_node(curr_node.right)
+	curr_node.right_total = right
+	return 1 + left + right
+
+def is_BST_1_helper(root):
+	pass
+def is_BST_1(root):
+	pass
 
 
-
-
-
-
-a = Binary_Tree()
-a.random_tree()
-a.pre_order_traverse()
-a.in_order_traverse()
-a.post_order_traverse()
-print(a.get_tree_height(a.root))
-traverse_by_level(a.root)
-b = upside_down(a.root)
-traverse_by_level(b)
+a = random_tree()
+num_of_left_node(a)
+num_of_right_node(a)
+# print(get_tree_size(a))
+# pre_order_traverse(a)
+# pre_order_iter(a)
+# in_order_traverse(a)
+# in_order_iter(a)
+# post_order_traverse(a)
+# post_order_iter(a)
+# traverse_by_level(a)
+# print(longest_consec(a, None, 0))
